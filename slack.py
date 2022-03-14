e28310a6c6da45cd7f3e5033ce97e0eaa632b18c
indyinternational <90900379+indyinternational@users.noreply.github.com>
2022-03-14 15:57:42 +0700

    Update slack.py

--- slack.py
+++ slack.py
@@ -17,7 +17,7 @@ for channel in channel_list:
 @app.get("/1")
 def get_status():
     """Get status of messaging server."""
-    response_API = requests.get('http://indyinter.duckdns.org:8081/authen/zero/1234)
+    response_API = requests.get('http://indyinter.duckdns.org:8081/authen/zero/1234')
     data = response_API.text
     parse_json = json.loads(data)
     return parse_json
