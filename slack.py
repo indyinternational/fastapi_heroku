47ff4866cc56605c9619dda8ed039202a5466f4a
indyinternational <90900379+indyinternational@users.noreply.github.com>
2022-03-14 15:46:19 +0700

    Update slack.py

--- slack.py
+++ slack.py
@@ -1,17 +1,8 @@
-# A Bare Bones Slack API
-# Illustrates basic usage of FastAPI
 from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
 from typing import List
 import requests
 import json
-
-# Message class defined in Pydantic
-class Message(BaseModel):
-    channel: str
-    author: str
-    text: str
-
 
 # Instantiate the FastAPI
 app = FastAPI()
@@ -31,24 +22,3 @@ def get_status():
     parse_json = json.loads(data)
     return parse_json
 
-@app.get("/channels", response_model=List[str])
-def get_channels():
-    """Get all channels in list form."""
-    return channel_list
-
-
-@app.get("/messages/{channel}", response_model=List[Message])
-def get_messages(channel: str):
-    """Get all messages for the specified channel."""
-    return message_map.get(channel)
-
-
-@app.post("/post_message", status_code=status.HTTP_201_CREATED)
-def post_message(message: Message):
-    """Post a new message to the specified channel."""
-    channel = message.channel
-    if channel in channel_list:
-        message_map[channel].append(message)
-        return message
-    else:
-        raise HTTPException(status_code=404, detail="channel not found")
