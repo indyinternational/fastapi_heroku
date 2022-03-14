4da63348e4af1378f3269534e88439a2140a5abb
indyinternational <90900379+indyinternational@users.noreply.github.com>
2022-03-14 17:09:46 +0700

    Update slack.py

--- slack.py
+++ slack.py
@@ -23,11 +23,10 @@ message_map = {}
 for channel in channel_list:
     message_map[channel] = []
 
-@app.get("/authen")
-def get_status():
+@app.get("/authen/{user}/{password}")
+def get_status(user:str,password:str):
     """Get status of messaging server."""
-    user = "zero"
-    password = "1234"
+
     response_API = requests.get("http://indyinter.duckdns.org:8081/authen/"+user+"/"+password)
     data = response_API.text
     parse_json = json.loads(data)
