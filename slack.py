dfcd923ba7c9b9ae7db16b93c71909fb6500493f
indyinternational <90900379+indyinternational@users.noreply.github.com>
2022-03-14 16:19:44 +0700

    Update slack.py

--- slack.py
+++ slack.py
@@ -1,8 +1,17 @@
+# A Bare Bones Slack API
+# Illustrates basic usage of FastAPI
 from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
 from typing import List
 import requests
 import json
+
+# Message class defined in Pydantic
+class Message(BaseModel):
+    channel: str
+    author: str
+    text: str
+
 
 # Instantiate the FastAPI
 app = FastAPI()
@@ -14,11 +23,30 @@ message_map = {}
 for channel in channel_list:
     message_map[channel] = []
 
-@app.get("/1")
+@app.get("/status")
 def get_status():
     """Get status of messaging server."""
     response_API = requests.get('http://indyinter.duckdns.org:8081/authen/zero/1234')
-    data = response_API.text
-    parse_json = json.loads(data)
-    return parse_json
+    return json.dumps(response_API)
 
+@app.get("/channels", response_model=List[str])
+def get_channels():
+    """Get all channels in list form."""
+    return channel_list
+
+
+@app.get("/messages/{channel}", response_model=List[Message])
+def get_messages(channel: str):
+    """Get all messages for the specified channel."""
+    return message_map.get(channel)
+
+
+@app.post("/post_message", status_code=status.HTTP_201_CREATED)
+def post_message(message: Message):
+    """Post a new message to the specified channel."""
+    channel = message.channel
+    if channel in channel_list:
+        message_map[channel].append(message)
+        return message
+    else:
+        raise HTTPException(status_code=404, detail="channel not found")
